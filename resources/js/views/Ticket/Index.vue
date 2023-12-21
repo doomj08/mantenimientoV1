8679ea9cb0531c103b696c4288f694b3c289f8f6
doomj08 <34778393+doomj08@users.noreply.github.com>
2023-12-20 19:01:30 -0500

    update tickets

--- resources/js/views/Ticket/Index.vue
+++ resources/js/views/Ticket/Index.vue
@@ -7,88 +7,49 @@
         </template>
     </EncabezadoVue>
     
-    <div class="flex flex-col">
-    <div class="overflow-x-auto">
-        <div class="inline-block min-w-full align-middle">
-            <div class="overflow-hidden shadow">
-                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
-                    <thead class="bg-gray-100 dark:bg-gray-700">
-                        <tr>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                N° 
-                            </th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                Fecha / Hora
-                            </th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                Descripción
-                            </th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                Cliente 
-                            </th>
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                Estado 
-                            </th>
+    
 
-                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                Actions
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                        
-                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" v-for="(ticket,index) in tickets" :key="index">
+        <table class="w-full table-fixed border-separate border-spacing-2 border border-slate-400">
+  <thead>
+    <tr>
+      <th>N°</th>
+      <th>Fecha / Hora</th>
+      <th>Descripción</th>
+      <th>Cliente</th>
+      <th>Estado</th>
+      <th>Actions</th>
 
-                            <td class="flex items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                <div class="text-sm font-normal text-gray-500 dark:text-gray-400">
-                                    <div class="text-base font-semibold text-gray-900 dark:text-white">{{ ticket.num_ticket }}</div>
-                                </div>
-                            </td>
-                            <td class=" items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                <div class="text-sm font-normal text-gray-300 dark:text-gray-200">
-                                    <div class="text-base font-semibold text-gray-600 dark:text-white">{{ ticket.fecha_hora }}</div>
-                                </div>
-                            </td>
-                            <td class=" items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                <div class="text-sm font-normal text-gray-300 dark:text-gray-200">
-                                    <div class="text-base font-semibold text-gray-600 dark:text-white">{{ ticket.descripcion }}</div>
-                                </div>
-                            </td>
-                            <td class=" items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                <div class="text-sm font-normal text-gray-300 dark:text-gray-200">
-                                    <div class="text-base font-semibold text-gray-600 dark:text-white">{{ ticket.cliente.nombre }}</div>
-                                </div>
-                            </td>
-                            <td class=" items-center p-4 mr-12 space-x-6 whitespace-nowrap">
-                                <div class="text-sm font-normal text-gray-300 dark:text-gray-200">
-                                    <div class="text-base font-semibold text-gray-600 dark:text-white">{{ (ticket['estado-ticket']>0)?'Cerrado':'Abierto' }}</div>
-                                </div>
-                            </td>
-                          
-                            <td class="p-4 space-x-2 whitespace-nowrap">
-                                <router-link  :to="{ name: 'ticket_servicios', params: { ticket_id:ticket.id, ticket_num:ticket.num_ticket }}" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="bg-white hover:bg-gray-100 dark:hover:bg-gray-700" v-for="(ticket,index) in tickets" :key="index">
+        <td>{{ ticket.num_ticket }}</td>
+        <td>{{ ticket.fecha_hora }}</td>
+        <td>{{ ticket.descripcion }}</td>
+        <td>{{ ticket.cliente.nombre }}</td>
+        <td>{{ (ticket['estado-ticket']>0)?'Cerrado':'Abierto' }}</td>
+        <td class="grid space-y-1 whitespace-nowrap">
+            <router-link  :to="{ name: 'ticket_servicios', params: { ticket_id:ticket.id, ticket_num:ticket.num_ticket }}" class=" inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                                     v-if="(ticket['estado-ticket']<ticket.servicio_count || ticket.servicio_count==0 )"
                                 >
                                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path><path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path></svg>
                                     Detalles Ticket
                                 </router-link>
-                                <EditForm @update="getTickets()" class="text-left" :id="ticket.id" v-if="(!ticket['estado-ticket']>0)"/>
+                                <EditForm @update="getTickets()" class=" text-left" :id="ticket.id" v-if="(!ticket['estado-ticket']>0)"/>
 
-                                <DeleteForm  @update="getTickets()" :name="ticket.nombre" url="tickets" :id="ticket.id" v-if="(!ticket['estado-ticket']>0)"/>
-                                <router-link  :to="{ name: 'ticket_pdf', params: { ticket_id:ticket.id }}">Ver PDF</router-link>
-                                                                 
-                            </td>
-                        </tr>
-                        
-                        
-                        
-                        
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
+                                <DeleteForm  @update="getTickets()" class=" " :name="ticket.nombre" url="tickets" :id="ticket.id" v-if="(!ticket['estado-ticket']>0)"/>
+                                <router-link class="  text-center"  :to="{ name: 'ticket_pdf', params: { ticket_id:ticket.id }}">Ver PDF</router-link>
+      </td>
+    </tr>
+
+  </tbody>
+</table>
+            
+
+            
+        
+
+
 </template>
 
 <script setup>
