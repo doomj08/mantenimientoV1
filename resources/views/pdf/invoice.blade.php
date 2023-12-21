9c02230a80b10d117c2851e8f4c92d1b88bd7083
doomj08 <34778393+doomj08@users.noreply.github.com>
2023-12-20 20:04:48 -0500

    Update invoice.blade.php

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -1,19 +1,19 @@
 <style>
 
-  @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@500&display=swap');
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
 
-    
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@500&display=swap');
     /* Añado la declaración de font-family, para usar la fuente de Google Fonts en este PDF */
     
     h1 {
-        font-family: 'Roboto Condensed', sans-serif;
-        color: 303030;
+        font-family: 'Roboto Flex', sans-serif;
+        color: #303030;
         font-size: 20px;
     }
     body {
+        font-family: 'Roboto Flex', sans-serif;
         
-        font-family: 'Roboto Condensed', sans-serif;
-        color: 303030;
+        color: #303030;
         font-size: 12px;
     }
 
