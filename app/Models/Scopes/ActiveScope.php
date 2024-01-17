9002a00a92fe05737a01869650a33b7ce0e738b0
doomj08 <34778393+doomj08@users.noreply.github.com>
2024-01-17 10:02:01 -0500

    add Scope Global

---
+++ app/Models/Scopes/ActiveScope.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+
+class ActiveScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     */
+    public function apply(Builder $builder, Model $model): void
+    {
+        if(auth()->user()){
+            $empresa_id=auth()->user()->empresa_id;
+            $builder->where('empresa_id',$empresa_id );
+        }
+        else{
+            $empresa_id=null;
+            //$builder->where('empresa_id',$empresa_id );
+        }
+        
+    }
+}
--- app/Models/TipoArticulo.php
+++ app/Models/TipoArticulo.php
@@ -2,12 +2,27 @@
 
 namespace App\Models;
 
+use App\Models\Scopes\ActiveScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class TipoArticulo extends Model
 {
     use HasFactory;
 
     protected $fillable=['tipo'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new ActiveScope);
+        if(auth()->user())
+            $empresa_id=auth()->user()->empresa_id;
+        else
+            $empresa_id=null;
+
+    }
+
+    
 }
