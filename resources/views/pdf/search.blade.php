1f76a2590924fc85aa883a62d83a510bc81468dc
doomj08 <34778393+doomj08@users.noreply.github.com>
2024-01-30 14:33:26 -0500

    Update search.blade.php

--- resources/views/pdf/search.blade.php
+++ resources/views/pdf/search.blade.php
@@ -158,6 +158,13 @@
     </div>
 
     <script>
+        let token = document.head.querySelector('meta[name="csrf-token"]');
+
+        if (token) {
+        window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+        } else {
+        console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+        }
         const { createApp, ref } = Vue
         createApp({
             data() {
