5e9a60df3acde9733f1a8c856701c53b91ae9a52
doomj08 <34778393+doomj08@users.noreply.github.com>
2024-08-29 11:24:31 -0500

    Update ServicioArticulo.php

--- app/Models/ServicioArticulo.php
+++ app/Models/ServicioArticulo.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ServicioArticulo extends Model
 {
     use HasFactory;
+
+    public function Servicio(){
+        return $this->belongsTo('App\Models\Servicio');
+    }
 }
